615d1e6358c877fee0c6fb1c59ace5d7583f93e5
adam aulia rahmadi <adamaulia242@gmail.com>
2015-04-26 02:14:29 +0700

    tes commit 2
    disuruh wiwra

--- Java/ANN/Ann/src/ann/Ann.java
+++ Java/ANN/Ann/src/ann/Ann.java
@@ -17,6 +17,7 @@ public class Ann {
     public static void main(String[] args) {
         // TODO code application logic here
 		System.out.println("Hello ANN");
+		System.out.println("Hello ANN disuruh wira");
     }
     
 }
