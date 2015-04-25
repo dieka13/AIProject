cacf8126662ab3c64bc295cc0ad6c393879cc724
arifpz <arif.p.zati@hotmail.com>
2015-04-26 01:26:54 +0700

    ann first commit

---
+++ Java/ANN/src/com/ann/function/intiation.java
@@ -0,0 +1,10 @@
+package com.ann.function;
+
+public class intiation {
+	
+	/*
+	 * This file will contain read input data and initiation variable for ANN process
+	 */
+	
+	
+}
---
+++ Java/ANN/src/com/ann/main/Main.java
@@ -0,0 +1,9 @@
+package com.ann.main;
+
+public class Main {
+	
+	public static void main(String [] args){
+		
+	}
+	
+}
